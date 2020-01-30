380208129ce22176662c03bce321c609d656c269
InstaDeliveryk <neo_vinch@mail.ru>
2020-01-30 13:08:13 +0600

    My Django Girls app, first commit

--- curier/models.py
+++ curier/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-from phonenumber_field.modelfields import PhoneNumberField
+
 class Curier(models.Model):
 	user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,related_name="mycurier")
 	date_of_birth=models.DateField(blank=True, null=True)
